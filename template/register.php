abab51618941e96c53c047d2b5303125a314f55f
Etienne Blanc-Coquand <etienne1296pro@gmail.com>
2017-11-12 19:34:17 +0100

    Delete comments

--- template/register.php
+++ template/register.php
@@ -1,9 +1,4 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: admin
-     * Date: 10/11/2017
-     * Time: 09:57
-     */
     require_once "../forms/form_register.php";
     require_once "../db/db.php";
+
