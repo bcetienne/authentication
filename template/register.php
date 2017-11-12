c5acdb2881b7fd70c161261f76db0a1c92ed393b
Etienne Blanc-Coquand <etienne1296pro@gmail.com>
2017-11-12 17:34:15 +0100

    Registration file added

---
+++ template/register.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: admin
+     * Date: 10/11/2017
+     * Time: 09:57
+     */
+    require_once "../forms/form_register.php";
+    require_once "../db/db.php";
