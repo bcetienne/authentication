b6f8f81e058466b5c07baf4f8565797de1a040a7
Etienne Blanc-Coquand <etienne1296pro@gmail.com>
2017-11-12 17:29:57 +0100

    Register form

---
+++ forms/form_register.php
@@ -0,0 +1,29 @@
+<?php
+    require_once "../template/head.php";
+    require_once "../template/foot.php";
+    /**
+     * Retourne le formulaire de création de compte
+     */
+    ?>
+<h1>Enregistrez-vous</h1>
+<form action="" method="post">
+    <label for="nom">Nom :</label>
+    <input id="nom" name="nom" type="text" placeholder="Dupont">
+    <br>
+    <label for="prenom">Prénom :</label>
+    <input id="prenom" name="prenom" type="text" placeholder="Kévin">
+    <br>
+    <label for="email">E-mail :</label>
+    <input id="email" name="email" type="email" placeholder="user@domain.com">
+    <br>
+    <label for="password">Mot de passe :</label>
+    <input id="password" name="password" type="password">
+    <br>
+    <label for="repassword">Resaisissez votre mot de passe :</label>
+    <input id="repassword" name="repassword" type="password">
+    <br>
+    <input name="submit" type="submit">
+    <input name="reset" type="reset">
+</form>
+<br>
+<small><a href="/">Retour au menu</a></small>
