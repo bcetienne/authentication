0a7d92ca0c2a3731df051e487fdc1c86f45c7c3b
Etienne Blanc-Coquand <etienne1296pro@gmail.com>
2017-11-12 17:31:04 +0100

    User page

---
+++ pages/user.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("./db/db.php");
+    ?>
+
+<h1>Bienvenue <?php echo implode(getName($_SESSION['user'][2])) ?></h1>
+<br>
+<a href=""><h2>Modifier vos informations personnelles</h2></a>
+<br>
+<a href=""><h2>Modifier votre mot de passe</h2></a>
+<br>
+<a href=""><h2>Ajouter une expérience professionnelle</h2></a>
+<br>
+<a href=""><h2>Ajouter une formation</h2></a>
+<br>
+<a href="../template/logout.php">Se déconnecter</a>
