64531ebe0cdcc0100ac4953803c703dc2a3badd9
Etienne Blanc-Coquand <etienne1296pro@gmail.com>
2017-11-12 17:31:53 +0100

    Head of html page

---
+++ template/head.php
@@ -0,0 +1,13 @@
+<?php ?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Authentification</title>
+</head>
+<body>
